27e704952b68496493cb2c130cd56d75b313fe4d
ancysaji <48525098+ancysaji@users.noreply.github.com>
2024-03-19 23:31:02 -0400

    Update test_area.py

--- test_area.py
+++ test_area.py
@@ -17,5 +17,5 @@ def test_calculate_area_square_accuracy():
     # Last two digits of student ID: 100925334 as the expected output
     student_id = "34"
     expected_output = int(student_id) ** 2
-    assert calculate_area_square(int(student_id)) == expected_output
+    assert calculate_area_square(student_id) == expected_output
 
