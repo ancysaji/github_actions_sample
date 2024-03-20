46d18a901e10e37a7647de5c799abc16a896722c
ancysaji <48525098+ancysaji@users.noreply.github.com>
2024-03-19 22:59:41 -0400

    Update test_area.py

--- test_area.py
+++ test_area.py
@@ -12,3 +12,10 @@ def test_calculate_area_square_string():
 def test_calculate_area_square_list():  
     with pytest.raises(TypeError):  
         calculate_area_square([2])
+
+def test_calculate_area_square_accuracy():
+    # Last two digits of student ID: 100925334 as the expected output
+    student_id = "34"
+    expected_output = int(student_id) ** 2
+    assert calculate_area_square(student_id) == expected_output
+
